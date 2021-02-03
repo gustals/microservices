096154d48116690c98a5e9210284d79a80ae0e3d
Gustavo Lopes <20181014040008>
2021-02-02 21:58:49 -0300

    creat eureka server project with spring-cliud-netflix-eureka; config eureka server and client's

--- payroll/src/main/java/com/microservices/payroll/PayrollApplication.java
+++ payroll/src/main/java/com/microservices/payroll/PayrollApplication.java
@@ -2,10 +2,11 @@ package com.microservices.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "worker")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class PayrollApplication {
