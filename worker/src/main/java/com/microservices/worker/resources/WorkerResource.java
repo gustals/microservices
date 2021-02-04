f8b1254177edbc5312f41dddac09e7f9b751bcd3
Gustavo Lopes <20181014040008>
2021-02-04 20:46:08 -0300

    create config server and conect with git repository config. update ms-worker for use config server profiles and add Spring actuator for management updates in config files runtime

--- worker/src/main/java/com/microservices/worker/resources/WorkerResource.java
+++ worker/src/main/java/com/microservices/worker/resources/WorkerResource.java
@@ -3,6 +3,8 @@ package com.microservices.worker.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.microservices.worker.entities.Worker;
 import com.microservices.worker.repositories.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	@Value("${test.config}")
+	private String testConfig;
 	
 	@Autowired
 	private WorkerRepository repository;
@@ -32,5 +38,9 @@ public class WorkerResource {
 		return ResponseEntity.ok(objWorker);
 	}
 	
+	@GetMapping(value = "/config")
+	public ResponseEntity<String> getConfig(){
+		return ResponseEntity.ok(testConfig);
+	}	
 	
 }
